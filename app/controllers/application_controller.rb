af5ac696ba1c6d7978af77231391f2ebc888445e
Yuyi <yuyi365@gmail.com>
2021-12-05 20:13:29 -0500

    commit tree 7df5a680e80594e5631a021d8a5181a5462023eb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,11 +9,46 @@ class ApplicationController < Sinatra::Base
   get '/games/:id' do
     game = Game.find(params[:id])
 
-    game.to_json(only: [:id, :title, :genre, :price], include: {
-      reviews: { only: [:comment, :score], include: {
-        user: { only: [:name] }
-      } }
-    })
+    game.to_json(
+      only: %i[id title genre price],
+      include: {
+        reviews: {
+          only: %i[comment score],
+          include: {
+            user: {
+              only: [:name],
+            },
+          },
+        },
+      },
+    )
   end
 
+  delete '/reviews/:id' do
+    # find the review using the ID
+    review = Review.find(params[:id])
+
+    # delete the review
+    review.destroy
+
+    # send a response with the deleted review as JSON
+    review.to_json
+  end
+
+  post '/reviews' do
+    review =
+      Review.create(
+        score: params[:score],
+        comment: params[:comment],
+        game_id: params[:game_id],
+        user_id: params[:user_id],
+      )
+    review.to_json
+  end
+
+  patch '/reviews/:id' do
+    review = Review.find(params[:id])
+    review.update(score: params[:score], comment: params[:comment])
+    review.to_json
+  end
 end
